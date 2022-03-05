832b004e6253517a066e449b53aad7a07b75427d
CorreiaJV <correia.jv16@gmail.com>
2022-03-05 19:11:47 -0300

    feat(#49): Inicio da homePage foi adicionado o componente card e a chamada da página
    Co-authored-by:Matheus Perillo <c4tr4u1@gmail.com>

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ChakraProvider } from '@chakra-ui/react'
+import { BrowserRouter } from "react-router-dom";
+
 import './index.css';
-import App from './App';
+import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ChakraProvider>
+    <BrowserRouter>
     <App />
-  </React.StrictMode>,
+    </BrowserRouter>
+  </ChakraProvider>,
   document.getElementById('root')
 );
 
