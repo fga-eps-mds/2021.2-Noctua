1a7d48becc5766c03add2b6dd19513341974f40b
CorreiaJV <correia.jv16@gmail.com>
2022-03-05 21:37:58 -0300

    feat(#49): Adição de sistema de rotas

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import Home from './Home';
 
-import Routes from './Routes';
+import {Route,Routes } from 'react-router-dom';
+import Router from './Router';
 
-function App() {
+
+const  App=()=> {
   return (
-      <>
-   
-    <Home />
-      </>
+      <Router/>
   );
 }
 
