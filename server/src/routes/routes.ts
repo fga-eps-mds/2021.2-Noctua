d802d63a45e54b317cc3c198972a454573714239
CorreiaJV <correia.jv16@gmail.com>
2022-03-06 20:20:25 -0300

    feat(#51): Adição de ednpoint para envio de e-mail
    Co-authored-by: Pedro Nogueira <190094486@aluno.unb.br>

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -1,5 +1,16 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
+import EmailController from '../controller/EmailController';
 
 const router = Router();
+const emailController = new EmailController();
+
+router.post("/enviaEmail",(req:Request, res:Response) =>{
+emailController.enviaEmail(req,res);
+})
+
+router.get("/emails",(req:Request, res:Response) =>{
+    emailController.getEmails(req,res);
+    })
+
 
 export default router;
