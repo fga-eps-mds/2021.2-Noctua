f04e0c63f989f9ee3c1caa169c5d9bdcb403433d
fellipepcs <fellipy.02@hotmail.com>
2022-04-04 20:08:02 -0300

    feat(#106)Adicionando salariodosmagistrados no schema mongodb

--- server/src/controller/SalarioDosMagistradosController.ts
+++ server/src/controller/SalarioDosMagistradosController.ts
@@ -2,6 +2,7 @@ import { query, Request, Response } from "express";
 import fs from 'mz/fs.js'
 import * as csv from '@fast-csv/parse'
 import path from 'path'
+import { SalarioDosMagistrados } from "../model/SalarioDosMagistrados";
 
 
 export default class SalarioDosMagistradosController {
@@ -9,3 +10,15 @@ export default class SalarioDosMagistradosController {
 async index(req: Request, res: Response):Promise<Response> {
     const csvpath = path.resolve('src/datasets', "/salario_dos_magistrados.csv");
     const log = (data:any) => console.log(JSON.stringify(data, undefined, 2));
+    try{
+        let headers = Object.keys(SalarioDosMagistrados.schema.paths)
+          .filter(k => ['_id','__v'].indexOf(k) === -1);
+    
+        console.log(headers);
+    
+        await new Promise((resolve,reject) => {
+    
+          let buffer:any = [],
+              counter = 0;
+    
+    }
