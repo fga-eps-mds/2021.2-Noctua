65bb745bfaad5635a7a504fb85dba95f4aa567f3
CorreiaJV <correia.jv16@gmail.com>
2022-04-22 16:46:43 -0300

    fix(#132):  Mudança aquivo config do server

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,7 @@ import databaseConnect from "./config/database";
 
 const app = express();
 
-const PORT = process.env.APP_PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 databaseConnect();
 
