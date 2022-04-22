42f488abee56a0dc6cf40ac0cd0d81a9b58b7c1b
CorreiaJV <correia.jv16@gmail.com>
2022-04-22 16:56:03 -0300

    fix(#132):  Ajuste deploy dockerfile

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,20 +3,31 @@ import  cors from 'cors';
 import router from "./routes/routes";
 import databaseConnect from "./config/database";
 
-const app = express();
 
-const PORT = process.env.PORT || 5000;
 
-databaseConnect();
+async function init (){
+    console.log("inicializando");
+    try {
+        const app = express();
 
-app.use(cors());
+    const PORT = process.env.PORT || 5000;
+    
+    await databaseConnect();
+    
+    app.use(cors());
+    
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    
+    //Carrega a rota
+    app.use("/", router);
+    
+    app.listen(PORT, () => {
+        console.log(`O servidor está rodando na porta ${PORT}`);
+    });
+    } catch (error) {
+        console.log(error);
+    }    
+}
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-//Carrega a rota
-app.use("/", router);
-
-app.listen(PORT, () => {
-    console.log(`O servidor está rodando na porta ${PORT}`);
-});
+init();
